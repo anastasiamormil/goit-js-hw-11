d2b66a3bc4a85854ffc2253c8c21a129fcebc5f3
Anastasia <mormilanastasia5@gmail.com>
2025-09-17 19:26:40 +0200

    Change project name

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+axios({
+    params: {
+        _limit: 10,
+        _page: 3;
+    }
+})
+    .then(res => {
+        console.log(res);
+    })
+    .catch(error => {
+     console.log(error);
+     
+    })
+function createMarkup(arr) {
+    return arr.map({ previewURL, tags } =>
+        `<li>
+            <img src = "${previewURL} 
+         </li>
+      )
+ }
