e8138e7b346092542e74abf430742984754d2900
Anastasia <mormilanastasia5@gmail.com>
2025-09-22 20:53:39 +0200

    Fixes

--- src/main.js
+++ src/main.js
@@ -17,38 +17,39 @@ function handleSubmit(event) {
   const query = input.value.trim().toLowerCase();
   console.log('input.value:', input.value);
   console.log('query:', query);
-  if (query) {
-    showLoader();
-    clearGallery();
-    getImagesByQuery(query)
-      .then(images => {
-        if (!images.length) {
-          iziToast.error({
-            title: 'Sorry',
-            message:
-              'There are no images matching your search query. Please try again!',
-            position: 'center',
-          });
-          return;
-        }
-
-        createGallery(images);
-      })
-      .catch(error => {
-        console.error('Pixabay API error:', error);
-        iziToast.error({
-          title: 'Error',
-          message: 'Something went wrong while fetching images',
-          position: 'center',
-        });
-      })
-      .finally(() => {
-        hideLoader();
-      });
-  } else {
+  if (!query) {
     iziToast.warning({
       title: 'Caution',
       message: 'Please enter key word',
     });
+    return;
   }
+
+  showLoader();
+  clearGallery();
+  getImagesByQuery(query)
+    .then(images => {
+      if (!images.length) {
+        iziToast.error({
+          title: 'Sorry',
+          message:
+            'There are no images matching your search query. Please try again!',
+          position: 'center',
+        });
+        return;
+      }
+
+      createGallery(images);
+    })
+    .catch(error => {
+      console.error('Pixabay API error:', error);
+      iziToast.error({
+        title: 'Error',
+        message: 'Something went wrong while fetching images',
+        position: 'center',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
 }
